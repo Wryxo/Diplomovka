a2a4174b936cd08a7df36a76855608c53539f8f8
Martin Strapko <wryxsk@gmail.com>
2015-12-09 11:00:42 +0100

    Pridany upper case, fixnute pocitanie pravdepodobnosti

--- CFgrammar.py
+++ CFgrammar.py
@@ -4,7 +4,7 @@ from collections import deque
 
 def prepareQueue(maxlength, maxsize):
 	# priprav queue, vytvor vsetky slova pre zakladne neterminaly
-	for i in range(1, maxsize):
+	for i in range(1, maxsize+1):
 		x = 'A'+str(i)
 		queue.append((x, maxlength-i))
 		if not x in rulez['Z']:
@@ -20,7 +20,7 @@ def prepareQueue(maxlength, maxsize):
 			rulez['Z'][x] = 1
 		if not x in rulez:
 			rulez[x] = {}
-			for z in product(alpha, repeat=i):
+			for z in product(digit, repeat=i):
 				s = ''.join(z)
 				rulez[x][s] = 1
 		x = 'S'+str(i)
@@ -29,7 +29,7 @@ def prepareQueue(maxlength, maxsize):
 			rulez['Z'][x] = 1
 		if not x in rulez:
 			rulez[x] = {}
-			for z in product(alpha, repeat=i):
+			for z in product(special, repeat=i):
 				s = ''.join(z)
 				rulez[x][s] = 1
 
@@ -37,12 +37,12 @@ def createRules(maxsize):
 	# dokym mas co, vytvaraj vsetky mozne zlozene neterminaly
 	while queue:
 		tmp = queue.popleft()
-		for i in range(1,maxsize):
+		for i in range(1,maxsize+1):
 			if tmp[1]-i < 0:
 				break
 			else:
 				if tmp[0][-2] == 'A':
-					if tmp[0][-1] == str(maxsize-1):
+					if tmp[0][-1] == str(maxsize):
 						x = tmp[0]+'A'+str(i)
 						queue.append((x, tmp[1]-i))
 						rulez['Z'][x] = 1
@@ -51,7 +51,7 @@ def createRules(maxsize):
 					queue.append((x, tmp[1]-i))
 					rulez['Z'][x] = 1
 				if tmp[0][-2] == 'D':
-					if tmp[0][-1] == str(maxsize-1):
+					if tmp[0][-1] == str(maxsize):
 						x = tmp[0]+'D'+str(i)
 						queue.append((x, tmp[1]-i))
 						rulez['Z'][x] = 1
@@ -60,7 +60,7 @@ def createRules(maxsize):
 					queue.append((x, tmp[1]-i))
 					rulez['Z'][x] = 1
 				if tmp[0][-2] == 'S':
-					if tmp[0][-1] == str(maxsize-1):
+					if tmp[0][-1] == str(maxsize):
 						x = tmp[0]+'S'+str(i)
 						queue.append((x, tmp[1]-i))
 						rulez['Z'][x] = 1
@@ -115,43 +115,48 @@ def updateMyrules(word):
 	rulez['Z'][rule] += 1
 	ruleCount['Z'] += 1
 
-maxlength = 4
+maxlength = 8
 maxsize = 4
-koeficient = 10000
-alpha = 'abcdefghijklmnopqrstuvwxyz'
+koeficient = 1
+alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 digit = '0123456789'
 special = "'~!@#$%^&*;,.-<>`_ "
 queue = deque([])
 rulez = {}
 rulez['Z'] = {}
-prepareQueue(14,5)
-createRules(5)
+prepareQueue(maxlength,maxsize)
+createRules(maxsize)
 ruleCount={}
 for rule in rulez:
 	ruleCount[rule] = len(rulez[rule])
 #print(ruleCount)
-with open('johnLC.txt') as f:
+with open('john.txt') as f:
+	for line in f:
+		word = line.rstrip('\n')
+		if len(word) > 0:
+			updateMyrules(word)
+with open('cain.txt') as f:
 	for line in f:
 		word = line.rstrip('\n')
 		if len(word) > 0:
 			updateMyrules(word)
 
-rc = sum(ruleCount.values())
+#rc = sum(ruleCount.values())
 for rule in rulez:
 	for r in rulez[rule]:
 		x = rulez[rule][r]
-		rulez[rule][r] = float((x / rc) * koeficient)
+		rulez[rule][r] = float((x / ruleCount[rule]) * koeficient)
 
 for x in rulez:
 	rulez[x] = sorted(rulez[x].items(), key=itemgetter(1), reverse=True)
 
-#print(ruleCount)
-#for rule in rulez:
-	#print(rule + ":")
-	#for r in rulez[rule]:
-	#	print("\t"+str(r[0])+":"+"{0:.4f}".format(r[1]))
-	#print()
-	#print()
+print(ruleCount)
+for rule in rulez:
+	print(rule + ":")
+	for r in rulez[rule]:
+		print("\t"+str(r[0])+"-"+"{0:.4f}".format(r[1]))
+	print()
+	print()
 
 class Neterminal:
 	deti=[]
@@ -202,11 +207,11 @@ nets=[]
 for r in rulez['Z']:
 	nets.append(Neterminal(r[0], [0] * (len(str(r[0]))//2)))
 
-print(nets[0].net)
-for i in range(0, 100):
-	print(nets[0].chance)
-	print(nets[0].next())
-	print()
+#print(nets[0].net)
+#for i in range(0, 200):
+#	print(nets[0].chance)
+#	print(nets[0].next())
+#	print()
 #curr = 0
 #for i in range(0,10):
 #	cc = nets[curr].chance * rulez['Z'][curr][1]
--- poradieGenerovania.py
+++ poradieGenerovania.py
@@ -1,9 +1,15 @@
 a = [200, 91, 82]
 b = [22, 20, 18, 9, 7]
+c = [47, 45, 42, 27, 11]
 res = []
 for i in range(0,3):
-	for j in range(0,3):
+	for j in range(0,5):
 		for k in range(0,5):
-			res.append((i,j,k,a[i]*a[j]*b[k]))
+			res.append((i,j,k,a[i]*b[j]*c[k]))
 tres = sorted(res, key=lambda x: x[3], reverse=True)
-print(tres)
+for z in tres:
+	print(z)
+x = [0, 0, 0]
+
+#while x[0]<len(a)-1 and x[1]<len(a)-1 and x[2]<len(b)-1:
+	#print(a[x[0]]*a[x[0]]*a[x[0]] + " " + str(x))
